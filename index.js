7f188976ac99f047cfb83a193168e3dde29b850e
nmhummel <natalie.hummel@gmail.com>
2021-01-28 20:13:50 -0600

    commit tree afd9aab329c9b477db7c877ec91e41e7d5a1d08a

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// Write your code here!
+main.remove() 
+
+let newHeader = document.createElement("h1");
+newHeader.id = "victory";
+newHeader.innerHTML = "Flatiron School is the champion!";
+newHeader.className = "victory"
